22990e539ed21b8b64600bae2fa2178398e1eb2f
Kerri Sexton <k.sexton804@gmail.com>
2021-09-08 21:16:24 -0400

    added data

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO `Games` ( `gameName`, `console`, `yearPublished`, `paid`)
+VALUES( 'Legend of Zelda Ocarina of Time', 'N64', '1998', '65.00');
+
+INSERT INTO `Games` ( `gameName`, `console`, `yearPublished`, `paid`)
+VALUES( 'Legend of Zelda Majoras Mask', 'N64', '2000', '0.00');
+
+INSERT INTO `Games` ( `gameName`, `console`, `yearPublished`, `paid`)
+VALUES( 'Super Mario 64', 'N64', '1996', '40.00');
+
+INSERT INTO `Games` ( `gameName`, `console`, `yearPublished`, `paid`)
+VALUES( 'Star Fox 64', 'N64', '1997', '3.99');
+
+INSERT INTO `Games` ( `gameName`, `console`, `yearPublished`, `paid`)
+VALUES( 'Star Fox 64', 'N64', '1997', '3.99');
+
+INSERT INTO `Games` ( `gameName`, `console`, `yearPublished`, `paid`)
+VALUES( 'Mario Party', 'N64', '1998', '3.99');
+
+INSERT INTO `Games` ( `gameName`, `console`, `yearPublished`, `paid`)
+VALUES( 'Mario Party 2', 'N64', '1999', '3.99');
+
+INSERT INTO `Games` ( `gameName`, `console`, `yearPublished`, `paid`)
+VALUES( 'Mario Party 3', 'N64', '2000', '3.99');
+
+INSERT INTO `Games` ( `gameName`, `console`, `yearPublished`, `paid`)
+VALUES( 'Paper Mario', 'N64', '2001', '0.00');
+
+INSERT INTO `Games` ( `gameName`, `console`, `yearPublished`, `paid`)
+VALUES( 'Conkers Bad Fur Day', 'N64', '2001', '0.00');
